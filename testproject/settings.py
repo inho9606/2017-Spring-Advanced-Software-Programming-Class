da41578d007ae2b82955ec8e615c48193272f6fa
inho9606 <ino96@sogang.ac.kr>
2017-06-29 23:44:15 +0900

    Adding register feature and home page

--- testproject/settings.py
+++ testproject/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = '/accounts/login/'
+LOGOUT_URL = '/accounts/logout/'
+LOGIN_REDIRECT_URL = '/'
--- testproject/urls.py
+++ testproject/urls.py
@@ -21,6 +21,11 @@ from django.contrib import admin
 
 from bookmark.views import BookmarkLV, BookmarkDV
 
+from django.conf.urls.static import static
+from django.conf import settings
+from testproject.views import HomeView
+from testproject.views import UserCreateView, UserCreateDoneTV
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
@@ -28,4 +33,8 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^ipsi/', include('ipsi.urls', namespace='ipsi')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/register/$', UserCreateView.as_view( ), name='register'),
+    url(r'^accounts/register/done/$', UserCreateDoneTV.as_view( ), name='register_done'),
+    url(r'^$', HomeView.as_view( ), name='home'),
 ]
